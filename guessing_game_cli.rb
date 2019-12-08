36b17bec1c31ed13b0a93d655b3c4fc9dbcfcd5c
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-08 01:24:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,4 +1,6 @@
-
+# In this lab, you are tasked with building guessing game. The game is simple:
+# the computer will choose a random number between 1 and 6 and ask you to make a
+# guess. If you guess right, you win, if not, you lose.
 
 
 # Code your solution here!
